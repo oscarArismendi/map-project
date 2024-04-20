642c44c96d78e87f6bf7e93bb80396ba35a51eda
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-04-19 21:58:56 -0500

    feat: network shows all the levels

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 
 const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50,"AI");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50,"KEYS");
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50,"NPC",2)
 ];
--- js/visualizer.js
+++ js/visualizer.js
@@ -7,7 +7,13 @@ class Visualizer{
         const width = ctx.canvas.width-margin*2;
         const height = ctx.canvas.height-margin*2;
 
-        Visualizer.drawLevel(ctx,network.levels[0],left,top,width,height);
+        const levelHeight = height/network.levels.length;
+        for(let i = network.levels.length-1;i>= 0;i--){
+            const levelTop = top+lerp(height-levelHeight,0,network.levels.length==1?0.5:i/(network.levels.length-1));
+            Visualizer.drawLevel(ctx,network.levels[i],left,levelTop,width,levelHeight);
+
+        }
+
     }
 
     static drawLevel(ctx,level,left,top,width,height){
@@ -31,25 +37,39 @@ class Visualizer{
 
         for(let i = 0; i < inputs.length;i++){
             const x = Visualizer.#getNodeX(inputs,i,left,right);
+            
             ctx.beginPath();
             ctx.arc(x,bottom,nodeRadius,0,Math.PI*2);
-            ctx.fillStyle = "white";
+            ctx.fillStyle = "black";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(x,bottom,nodeRadius*0.6,0,Math.PI*2);
+            ctx.fillStyle = getRGBA(inputs[i]);
             ctx.fill();
             
         }
 
         for(let i = 0; i < outputs.length;i++){
             const x = Visualizer.#getNodeX(outputs,i,left,right);
+            
             ctx.beginPath();
             ctx.arc(x,top,nodeRadius,0,Math.PI*2);
-            ctx.fillStyle = "white";
+            ctx.fillStyle = "black";
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(x,top,nodeRadius*0.6,0,Math.PI*2);
+            ctx.fillStyle = getRGBA(outputs[i]);
             ctx.fill();
 
             ctx.beginPath();
             ctx.lineWidth = 2;
-            ctx.arc(x,top,nodeRadius,0,Math.PI*2);
+            ctx.arc(x,top,nodeRadius*0.8,0,Math.PI*2);
+            ctx.setLineDash([3,3]);
             ctx.strokeStyle= getRGBA(biases[i]);
             ctx.stroke();
+            ctx.setLineDash([]);
         }
 
 
